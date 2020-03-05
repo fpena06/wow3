5124dd2c1d343f46c8e5aa3dd40a36d14674c4f3
Shreeji Pedhadiya <shreejilucifer@gmail.com>
2020-03-05 18:38:09 +0530

    Great grand Ravi

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
-const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
-const routes = require("./routes");
-const config = require("config");
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const routes = require('./routes');
+const config = require('config');
 const app = express();
-// app;
-var server = require("http").Server(app);
-var io = require("socket.io")(server);
+const socketIo = require('socket.io');
+const http = require('http');
 
-app.use(function(req, res, next) {
-  io.on("connection", socket => {
-    console.log("I am Connected");
-  });
+// app;
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', socket => {
+  console.log(socket.handshake.address + ' Connected');
+
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+app.use((req, res, next) => {
   res.io = io;
   next();
 });
-app.disable("x-powered-by");
+
+app.disable('x-powered-by');
+
 app.use(express.json());
+
 mongoose.connect(
-  config.get("DBURL"),
+  config.get('DBURL'),
   {
     useFindAndModify: false,
     useCreateIndex: true,
@@ -28,18 +37,24 @@ mongoose.connect(
   },
   err => {
     if (err) {
-      console.log("Error Connecting Database");
+      console.log('Error Connecting Database');
       console.log(err);
       process.exit(1);
     }
+    console.log('DB Connected');
   }
 );
-app.get("/", (req, res) => {
-  res.send({ message: config.get("dburl") });
+app.get('/', (req, res) => {
+  res.send({ message: config.get('dburl') });
 });
-app.use("/user", routes.user);
-app.use("/admin", routes.admin);
-let port = process.env.PORT || 3000;
-app.listen(port, "localhost", err => {
+app.use('/user', routes.user);
+app.use('/admin', routes.admin);
+
+let port1 = process.env.PORT || 3001;
+let port2 = process.env.PORT || 4001;
+
+app.listen(port1, 'localhost', err => {
   if (err) console.log(err);
 });
+
+server.listen(port2, () => console.log(`Listening on port ${port2}`));
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,50 +1,50 @@
-const express = require("express");
-const User = require("../controllers/user");
-const Auth = require("../middleware/index");
+const express = require('express');
+const User = require('../controllers/user');
+const Auth = require('../middleware/index');
 let router = express.Router();
 
 // add user
 
-router.post("/addUser", User.addUser);
+router.post('/addUser', User.addUser);
 
 // login user
 
-router.post("/login/user", User.login);
+router.post('/login/user', User.login);
 
 //change password
 
-router.post("/changePassword", Auth.auth.checkToken, User.changePassword);
+router.post('/changePassword', Auth.auth.checkToken, User.changePassword);
 
 // user dashboard
 
-router.get("/dashboard", User.dashboard);
+router.get('/dashboard', User.dashboard);
 
 // user dashboard after selecting category
 
 router.get(
-  "/dashboard/:category",
+  '/dashboard/:category',
   Auth.auth.checkToken,
   User.dashboardCategory
 );
 
 // asdfghjkl
-router.get("/asdf", (req, res) => {
-  res.io.emit("tweet", { id: "1", text: "Heloo" });
+router.get('/asdf', (req, res) => {
+  res.io.emit('outgoing data', { num: 98 });
 });
 //user leaderboard
 
-router.get("/leaderboard", Auth.auth.checkToken, User.leaderboard);
+router.get('/leaderboard', Auth.auth.checkToken, User.leaderboard);
 
 // transaction
 
-router.post("/transaction", Auth.auth.checkToken, User.transaction);
+router.post('/transaction', Auth.auth.checkToken, User.transaction);
 
 // buying shares
 
-router.post("/company/buyShares", Auth.auth.checkToken, User.buyShares);
+router.post('/company/buyShares', Auth.auth.checkToken, User.buyShares);
 
 //selling shares
 
-router.post("/company/sellShares", Auth.auth.checkToken, User.sellShares);
+router.post('/company/sellShares', Auth.auth.checkToken, User.sellShares);
 
 module.exports = router;
