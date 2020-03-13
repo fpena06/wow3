0c12dbcc63ab50ad84afd458b30a19ad64390d3c
Nikhil <nick2000@gmail.com>
2020-03-13 13:26:28 +0530

    added get watchlist

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -450,7 +450,7 @@ exports.removeFromWatchlist = async (req, res) => {
   if (!checkWatchlist)
     return res.send({ message: "company not present in watchlist" });
   let newWatchlist = user.watchList.filter(
-    p => p.name.toLowerCase().toString() != company.name.toString()
+    p => p.name.toString() != company.name.toString()
   );
   await User.findByIdAndUpdate(user._id, {
     watchList: newWatchlist
